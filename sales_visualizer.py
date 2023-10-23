8fbbc92e453a22bcd2eff72e64ee28d386bb617c
Ozz-E <67159787+Ozz-E@users.noreply.github.com>
2023-10-23 16:36:55 +0100

    task 4
    Added region selector

--- sales_visualizer.py
+++ sales_visualizer.py
@@ -13,22 +13,42 @@ app = dash.Dash(__name__)
 
 # Define the layout of the app
 app.layout = html.Div([
-    html.H1("Sales Data Visualizer"),
+    html.H1("Sales Data Visualizer", className='header'),
+    dcc.RadioItems(
+        id='region-filter',
+        options=[
+            {'label': 'All', 'value': 'all'},
+            {'label': 'North', 'value': 'north'},
+            {'label': 'East', 'value': 'east'},
+            {'label': 'South', 'value': 'south'},
+            {'label': 'West', 'value': 'west'}
+        ],
+        value='all',
+        labelStyle={'display': 'block'},
+        className='radio-button'
+    ),
     dcc.Graph(id='sales-chart'),
-])
+], className='app-container')
 
+# Define callback to update the chart based on selected region
 @app.callback(
     Output('sales-chart', 'figure'),
-    [Input('sales-chart', 'relayoutData')]
+    [Input('sales-chart', 'relayoutData'), Input('region-filter', 'value')]
 )
-def update_sales_chart(relayoutData):
+def update_sales_chart(relayoutData, selected_region):
     global df
     # Sort the data by date
-    df['date'] = pd.to_datetime(df['date'])
-    df = df.sort_values(by='date')
+    if selected_region == 'all':
+        filtered_df = df
+    else:
+        filtered_df = df[df['region'] == selected_region]
+
+    # Sort the data by date
+    filtered_df['date'] = pd.to_datetime(filtered_df['date'])
+    filtered_df = filtered_df.sort_values(by='date')
 
     # Create a line chart with Plotly Express
-    fig = px.line(df, x='date', y='sales', labels={'date': 'Date', 'sales': 'Sales'})
+    fig = px.line(filtered_df, x='date', y='sales', labels={'date': 'Date', 'sales': 'Sales'})
 
     # Highlight the period before and after the price increase (January 15, 2021)
     if 'xaxis.range' in relayoutData:
